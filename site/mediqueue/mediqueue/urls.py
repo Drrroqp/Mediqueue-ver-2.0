db9cff3b0468a02f70ad18747e47010b3751c26e
nvygolov <134294685+nvygolov@users.noreply.github.com>
2024-03-31 20:36:35 +0500

    Update urls.py

--- site/mediqueue/mediqueue/urls.py
+++ site/mediqueue/mediqueue/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-
+    path('admin/', admin.site.urls),
+    path('', index),
+    path('register/', views.register_view, name='register'),
+    path('account/', account, name = 'account'), #личный кабинет
 ]
